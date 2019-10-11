a79eab9c4357235bfb19a9ee9971ae2bdad82c1f
crusiris <cruzsirys@gmail.com>
2019-10-11 11:21:29 -0300

    Configurando firebase en la APP

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCoEh1ZQVu4OqArDCNtTQBRP54goBRf6yg",
+    authDomain: "basededatos-redsocial.firebaseapp.com",
+    databaseURL: "https://basededatos-redsocial.firebaseio.com",
+    projectId: "basededatos-redsocial",
+    storageBucket: "basededatos-redsocial.appspot.com",
+    messagingSenderId: "292479744480",
+    appId: "1:292479744480:web:3e5b96fce63d7b607a1385"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
